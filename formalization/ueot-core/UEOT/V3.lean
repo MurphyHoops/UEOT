48bf6266820e379f184020c1ef83666cf94a8abd
MurphyHoops <86187309+MurphyHoops@users.noreply.github.com>
2026-09-10 03:59:46 +0800

    build(formalization): include P-PRED-03 source theorem

--- formalization/ueot-core/UEOT/V3.lean
+++ formalization/ueot-core/UEOT/V3.lean
@@ -27,3 +27,4 @@ import UEOT.V3.RecoveryProbability
 import UEOT.V3.InformationPacking
 import UEOT.V3.InformationEntropy
 import UEOT.V3.ConcreteHistoryMarkovization
+import UEOT.V3.PredictionUpdate
