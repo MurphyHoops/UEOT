cac9255b97010b010279be72a4f303ad866b7571
MurphyHoops <86187309+MurphyHoops@users.noreply.github.com>
2026-09-10 18:50:54 +0800

    chore(formalization): include P-INFO-01 entropy-bound bridge in V3 build

--- formalization/ueot-core/UEOT/V3.lean
+++ formalization/ueot-core/UEOT/V3.lean
@@ -26,6 +26,7 @@ import UEOT.V3.RecoveryDiscrete
 import UEOT.V3.RecoveryProbability
 import UEOT.V3.InformationPacking
 import UEOT.V3.InformationEntropy
+import UEOT.V3.InformationEntropyBound
 import UEOT.V3.ConcreteHistoryMarkovization
 import UEOT.V3.PredictionUpdate
 import UEOT.V3.DynamicsCrossScale
