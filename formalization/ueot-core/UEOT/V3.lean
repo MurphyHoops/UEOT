92331546fb749ee83f24acac543db1fef365133f
MurphyHoops <86187309+MurphyHoops@users.noreply.github.com>
2026-09-10 13:43:32 +0800

    build(formalization): include information-retention corollaries

--- formalization/ueot-core/UEOT/V3.lean
+++ formalization/ueot-core/UEOT/V3.lean
@@ -26,6 +26,7 @@ import UEOT.V3.RecoveryDiscrete
 import UEOT.V3.RecoveryProbability
 import UEOT.V3.InformationPacking
 import UEOT.V3.InformationStatistic
+import UEOT.V3.InformationRetention
 import UEOT.V3.InformationEntropy
 import UEOT.V3.ConcreteHistoryMarkovization
 import UEOT.V3.PredictionUpdate
