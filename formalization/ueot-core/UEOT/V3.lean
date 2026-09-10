358cc88ae0be564cff2da8f932f0d975e384fc03
MurphyHoops <86187309+MurphyHoops@users.noreply.github.com>
2026-09-10 14:37:11 +0800

    chore(formalization): import stationary viability trajectory realization

--- formalization/ueot-core/UEOT/V3.lean
+++ formalization/ueot-core/UEOT/V3.lean
@@ -17,6 +17,7 @@ import UEOT.V3.TotalVariation
 import UEOT.V3.VariationBridge
 import UEOT.V3.ViabilityKernel
 import UEOT.V3.ViabilityPath
+import UEOT.V3.ViabilityTrajectory
 import UEOT.V3.TVSpan
 import UEOT.V3.TVKernel
 import UEOT.V3.FiniteHistory
