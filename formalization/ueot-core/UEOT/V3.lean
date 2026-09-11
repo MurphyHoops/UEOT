7837b97a609dfec905e9607a22fef3ec6be6cd9d
MurphyHoops <86187309+MurphyHoops@users.noreply.github.com>
2026-09-11 11:05:26 +0800

    build(v3): import P-STAT-06 first-moment bridge

--- formalization/ueot-core/UEOT/V3.lean
+++ formalization/ueot-core/UEOT/V3.lean
@@ -23,6 +23,7 @@ import UEOT.V3.FiniteAlphabetPStat01
 import UEOT.V3.FiniteAlphabetPStat01Radius
 import UEOT.V3.HilbertMeanConcentration
 import UEOT.V3.HilbertMeanSecondMoment
+import UEOT.V3.HilbertMeanFirstMoment
 import UEOT.V3.VariationBridge
 import UEOT.V3.ViabilityKernel
 import UEOT.V3.ViabilityPath
