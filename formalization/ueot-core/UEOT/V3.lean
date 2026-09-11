149a6e6b60f62cb69dc6315eb2468b345bf10b40
MurphyHoops <86187309+MurphyHoops@users.noreply.github.com>
2026-09-11 08:41:59 +0800

    build(v3): import P-STAT-01 confidence radius

--- formalization/ueot-core/UEOT/V3.lean
+++ formalization/ueot-core/UEOT/V3.lean
@@ -19,6 +19,7 @@ import UEOT.V3.StatisticalDefect
 import UEOT.V3.FiniteAlphabetConcentration
 import UEOT.V3.FiniteAlphabetSampling
 import UEOT.V3.FiniteAlphabetPStat01
+import UEOT.V3.FiniteAlphabetPStat01Radius
 import UEOT.V3.VariationBridge
 import UEOT.V3.ViabilityKernel
 import UEOT.V3.ViabilityPath
