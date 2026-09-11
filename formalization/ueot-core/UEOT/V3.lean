bc4e36b95e36e04d0556df757096f4ad7b2ab946
MurphyHoops <86187309+MurphyHoops@users.noreply.github.com>
2026-09-11 08:03:30 +0800

    build(v3): import finite alphabet sampling

--- formalization/ueot-core/UEOT/V3.lean
+++ formalization/ueot-core/UEOT/V3.lean
@@ -17,6 +17,7 @@ import UEOT.V3.TotalVariation
 import UEOT.V3.TransportDefect
 import UEOT.V3.StatisticalDefect
 import UEOT.V3.FiniteAlphabetConcentration
+import UEOT.V3.FiniteAlphabetSampling
 import UEOT.V3.VariationBridge
 import UEOT.V3.ViabilityKernel
 import UEOT.V3.ViabilityPath
