ba7a97520a12b1f802aa9acb942e756dbc5773f7
MurphyHoops <86187309+MurphyHoops@users.noreply.github.com>
2026-09-11 09:03:54 +0800

    build(v3): import P-STAT-07 MMD transport

--- formalization/ueot-core/UEOT/V3.lean
+++ formalization/ueot-core/UEOT/V3.lean
@@ -21,6 +21,7 @@ import UEOT.V3.FiniteAlphabetConcentration
 import UEOT.V3.FiniteAlphabetSampling
 import UEOT.V3.FiniteAlphabetPStat01
 import UEOT.V3.FiniteAlphabetPStat01Radius
+import UEOT.V3.MMDTransport
 import UEOT.V3.VariationBridge
 import UEOT.V3.ViabilityKernel
 import UEOT.V3.ViabilityPath
