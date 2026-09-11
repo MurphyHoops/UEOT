a4e0db7ad6314a4b78fc8633ab2307fb565c03af
MurphyHoops <86187309+MurphyHoops@users.noreply.github.com>
2026-09-11 09:22:33 +0800

    build(v3): import P-STAT-07 source wrapper

--- formalization/ueot-core/UEOT/V3.lean
+++ formalization/ueot-core/UEOT/V3.lean
@@ -22,6 +22,7 @@ import UEOT.V3.FiniteAlphabetSampling
 import UEOT.V3.FiniteAlphabetPStat01
 import UEOT.V3.FiniteAlphabetPStat01Radius
 import UEOT.V3.MMDTransport
+import UEOT.V3.MMDTransportSource
 import UEOT.V3.VariationBridge
 import UEOT.V3.ViabilityKernel
 import UEOT.V3.ViabilityPath
