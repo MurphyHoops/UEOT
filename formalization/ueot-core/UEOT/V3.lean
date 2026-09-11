d244daa9cd19e7d7f97e911d89f72fd5e90ab8b5
MurphyHoops <86187309+MurphyHoops@users.noreply.github.com>
2026-09-11 09:40:22 +0800

    build(v3): import two-sided bounded-loss concentration

--- formalization/ueot-core/UEOT/V3.lean
+++ formalization/ueot-core/UEOT/V3.lean
@@ -23,6 +23,7 @@ import UEOT.V3.FiniteAlphabetPStat01
 import UEOT.V3.FiniteAlphabetPStat01Radius
 import UEOT.V3.FiniteCandidateDiscovery
 import UEOT.V3.BoundedLossSampling
+import UEOT.V3.BoundedLossTwoSided
 import UEOT.V3.VariationBridge
 import UEOT.V3.ViabilityKernel
 import UEOT.V3.ViabilityPath
