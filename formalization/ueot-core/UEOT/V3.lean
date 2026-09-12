b09c077923b7e3c9ae84852e7236fe6d933889c1
MurphyHoops <86187309+MurphyHoops@users.noreply.github.com>
2026-09-12 18:04:20 +0800

    chore(P-INFO-02): import averaged kernel Pinsker theorem

--- formalization/ueot-core/UEOT/V3.lean
+++ formalization/ueot-core/UEOT/V3.lean
@@ -117,6 +117,7 @@ import UEOT.V3.InformationCore
 import UEOT.V3.InformationPinsker
 import UEOT.V3.InformationKernelTV
 import UEOT.V3.InformationKernelKL
+import UEOT.V3.InformationKernelPinsker
 import UEOT.V3.MarkovBoundary
 import UEOT.V3.RecoveryDiscrete
 import UEOT.V3.RecoveryProbability
