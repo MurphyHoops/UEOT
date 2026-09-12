0f3a4357a831ebe4ce5c90c6563fb7fe81708e6d
MurphyHoops <86187309+MurphyHoops@users.noreply.github.com>
2026-09-12 21:13:52 +0800

    test(P-INFO-04): add binary posterior bridge to official graph

--- formalization/ueot-core/UEOT/V3.lean
+++ formalization/ueot-core/UEOT/V3.lean
@@ -130,6 +130,7 @@ import UEOT.V3.InformationDecoderFano
 import UEOT.V3.InformationDecoderConditionalFano
 import UEOT.V3.InformationConditionalBinary
 import UEOT.V3.InformationPInfo04Conditional
+import UEOT.V3.InformationBinaryPosterior
 import UEOT.V3.InformationStatistic
 import UEOT.V3.InformationEntropyBound
 import UEOT.V3.InformationDiscreteEntropy
