db676aff11131c063c66c463c04f860a08e35c21
MurphyHoops <86187309+MurphyHoops@users.noreply.github.com>
2026-09-12 11:36:17 +0800

    chore(pstat06): import source fiber Hoeffding module

--- formalization/ueot-core/UEOT/V3.lean
+++ formalization/ueot-core/UEOT/V3.lean
@@ -74,6 +74,7 @@ import UEOT.V3.HilbertMeanSourceCenteredFiber
 import UEOT.V3.HilbertMeanSourceActiveIntegrability
 import UEOT.V3.HilbertMeanPastActiveContinuation
 import UEOT.V3.HilbertMeanSourceIncrementIdentification
+import UEOT.V3.HilbertMeanSourceFiberHoeffding
 import UEOT.V3.MMDTransport
 import UEOT.V3.MMDTransportSource
 import UEOT.V3.AverageErrorTransport
