0dfa192ab9d63704845fa7dae0d1278867f3269f
MurphyHoops <86187309+MurphyHoops@users.noreply.github.com>
2026-09-12 20:28:40 +0800

    chore(P-INFO-04): expose conditional source theorem

--- formalization/ueot-core/UEOT/V3.lean
+++ formalization/ueot-core/UEOT/V3.lean
@@ -129,6 +129,7 @@ import UEOT.V3.InformationSharpFano
 import UEOT.V3.InformationDecoderFano
 import UEOT.V3.InformationDecoderConditionalFano
 import UEOT.V3.InformationConditionalBinary
+import UEOT.V3.InformationPInfo04Conditional
 import UEOT.V3.InformationStatistic
 import UEOT.V3.InformationEntropyBound
 import UEOT.V3.InformationDiscreteEntropy
