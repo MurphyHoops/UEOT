adcf60b525dfddceb0067c7a5d186723a488de06
MurphyHoops <86187309+MurphyHoops@users.noreply.github.com>
2026-09-12 09:50:42 +0800

    build(P-STAT-06): import source Doob statistic bridge

--- formalization/ueot-core/UEOT/V3.lean
+++ formalization/ueot-core/UEOT/V3.lean
@@ -63,6 +63,7 @@ import UEOT.V3.HilbertMeanPrefixFiltration
 import UEOT.V3.HilbertMeanDoobPrefixBridge
 import UEOT.V3.HilbertMeanPrefixFutureAssembly
 import UEOT.V3.HilbertMeanSplitStatistic
+import UEOT.V3.HilbertMeanDoobStatisticBridge
 import UEOT.V3.MMDTransport
 import UEOT.V3.MMDTransportSource
 import UEOT.V3.AverageErrorTransport
