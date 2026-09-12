51276d9d38657d92e65c0a06b32e877bbec85df3
MurphyHoops <86187309+MurphyHoops@users.noreply.github.com>
2026-09-12 08:34:59 +0800

    build(P-STAT-06): import centered-fiber bridge

--- formalization/ueot-core/UEOT/V3.lean
+++ formalization/ueot-core/UEOT/V3.lean
@@ -52,6 +52,7 @@ import UEOT.V3.HilbertMeanProductLaw
 import UEOT.V3.HilbertMeanIntegralContraction
 import UEOT.V3.HilbertMeanRangeWidth
 import UEOT.V3.HilbertMeanContinuationWidth
+import UEOT.V3.HilbertMeanCenteredFiber
 import UEOT.V3.MMDTransport
 import UEOT.V3.MMDTransportSource
 import UEOT.V3.AverageErrorTransport
