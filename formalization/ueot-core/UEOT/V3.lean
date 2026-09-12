8fa63fc33d8d787668b2544cc521bf29588bbd4a
MurphyHoops <86187309+MurphyHoops@users.noreply.github.com>
2026-09-12 23:18:42 +0800

    test(P-INFO-04): wire green binary bridge foundations into official graph

--- formalization/ueot-core/UEOT/V3.lean
+++ formalization/ueot-core/UEOT/V3.lean
@@ -130,6 +130,9 @@ import UEOT.V3.InformationDecoderFano
 import UEOT.V3.InformationDecoderConditionalFano
 import UEOT.V3.InformationConditionalBinary
 import UEOT.V3.InformationPInfo04Conditional
+import UEOT.V3.InformationBinaryPosterior
+import UEOT.V3.InformationFin2KL
+import UEOT.V3.InformationBinaryConditionalMean
 import UEOT.V3.InformationBernoulliKL
 import UEOT.V3.InformationEventBernoulli
 import UEOT.V3.InformationBernoulliFano
