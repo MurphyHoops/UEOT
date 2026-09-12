e843fb545e68949619678739b4e512b02f3aff7d
MurphyHoops <86187309+MurphyHoops@users.noreply.github.com>
2026-09-12 13:56:11 +0800

    build(pstat06): include Bochner-integrable source uncentered tail

--- formalization/ueot-core/UEOT/V3.lean
+++ formalization/ueot-core/UEOT/V3.lean
@@ -88,6 +88,7 @@ import UEOT.V3.HilbertMeanSourceFirstIncrement
 import UEOT.V3.HilbertMeanSourceTerminalDoob
 import UEOT.V3.HilbertMeanSourceAzumaAssembly
 import UEOT.V3.HilbertMeanSourceCenteredTail
+import UEOT.V3.HilbertMeanSourceUncenteredTail
 import UEOT.V3.MMDTransport
 import UEOT.V3.MMDTransportSource
 import UEOT.V3.AverageErrorTransport
