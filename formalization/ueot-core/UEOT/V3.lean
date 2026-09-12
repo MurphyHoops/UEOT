ec29279b1ff3ee3a755365f7a67ed6d727125f6f
MurphyHoops <86187309+MurphyHoops@users.noreply.github.com>
2026-09-12 12:10:17 +0800

    chore(pstat06): expose source clipped increment layer

--- formalization/ueot-core/UEOT/V3.lean
+++ formalization/ueot-core/UEOT/V3.lean
@@ -77,6 +77,7 @@ import UEOT.V3.HilbertMeanSourceIncrementIdentification
 import UEOT.V3.HilbertMeanSourcePastFiber
 import UEOT.V3.HilbertMeanSourceFiberHoeffding
 import UEOT.V3.HilbertMeanPastActiveClippedFiber
+import UEOT.V3.HilbertMeanSourceClippedIncrement
 import UEOT.V3.MMDTransport
 import UEOT.V3.MMDTransportSource
 import UEOT.V3.AverageErrorTransport
