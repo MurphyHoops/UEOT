81ffe9588e119662ccd24a23f76663368a199175
MurphyHoops <86187309+MurphyHoops@users.noreply.github.com>
2026-09-12 09:49:51 +0800

    build(P-STAT-06): import source split statistic

--- formalization/ueot-core/UEOT/V3.lean
+++ formalization/ueot-core/UEOT/V3.lean
@@ -62,6 +62,7 @@ import UEOT.V3.HilbertMeanDoobCore
 import UEOT.V3.HilbertMeanPrefixFiltration
 import UEOT.V3.HilbertMeanDoobPrefixBridge
 import UEOT.V3.HilbertMeanPrefixFutureAssembly
+import UEOT.V3.HilbertMeanSplitStatistic
 import UEOT.V3.MMDTransport
 import UEOT.V3.MMDTransportSource
 import UEOT.V3.AverageErrorTransport
