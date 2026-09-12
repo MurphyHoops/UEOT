4f82ac4b09b6d9e9166af986a1f3c77f7d7c5cb9
MurphyHoops <86187309+MurphyHoops@users.noreply.github.com>
2026-09-12 14:53:16 +0800

    build(pstat06): import raw feature source wrapper

--- formalization/ueot-core/UEOT/V3.lean
+++ formalization/ueot-core/UEOT/V3.lean
@@ -54,6 +54,7 @@ import UEOT.V3.HilbertMeanProductLaw
 import UEOT.V3.HilbertMeanFeatureProductBridge
 import UEOT.V3.HilbertMeanCommonSampleUnion
 import UEOT.V3.HilbertMeanSourceFeaturePullback
+import UEOT.V3.HilbertMeanSourceFeatureRaw
 import UEOT.V3.HilbertMeanIntegralContraction
 import UEOT.V3.HilbertMeanRangeWidth
 import UEOT.V3.HilbertMeanContinuationWidth
