769b65e65790de04359c4a8a5cb43a72a9b03c4c
MurphyHoops <86187309+MurphyHoops@users.noreply.github.com>
2026-09-15 01:08:47 +0800

    formal: import P-REC-04 survival drift layer

--- formalization/ueot-core/UEOT/V3.lean
+++ formalization/ueot-core/UEOT/V3.lean
@@ -126,6 +126,7 @@ import UEOT.V3.RecoveryDiscrete
 import UEOT.V3.RecoveryProbability
 import UEOT.V3.RecoveryHitting
 import UEOT.V3.RecoveryHittingNonnegative
+import UEOT.V3.RecoveryHittingDrift
 import UEOT.V3.InformationPacking
 import UEOT.V3.InformationEntropy
 import UEOT.V3.InformationSharpFano
