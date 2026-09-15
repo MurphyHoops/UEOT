a69d04f25ca941424b65cb937a2335aa9e701327
MurphyHoops <86187309+MurphyHoops@users.noreply.github.com>
2026-09-16 02:28:46 +0800

    formal(P-COMP-01): include complete nontrivial partition domain in root

--- formalization/ueot-core/UEOT.lean
+++ formalization/ueot-core/UEOT.lean
@@ -46,3 +46,4 @@ import UEOT.V3.FixedFitnessRecurrenceUniqueness
 import UEOT.V3.FixedFitnessConcentration
 import UEOT.V3.CompositionPathInformation
 import UEOT.V3.CompositionPathKernelProduct
+import UEOT.V3.CompositionFinitePartitions
