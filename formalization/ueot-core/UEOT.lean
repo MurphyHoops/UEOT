3823588e4be6842b3d52c8943f06ab8227c58a15
MurphyHoops <86187309+MurphyHoops@users.noreply.github.com>
2026-09-26 15:22:10 +0800

    proof(P-KL-05): import Girsanov path KL

--- formalization/ueot-core/UEOT.lean
+++ formalization/ueot-core/UEOT.lean
@@ -33,6 +33,7 @@ import UEOT.V3.CompositionWindow
 import UEOT.V3.CompositionParentInformation
 import UEOT.V3.PathKLChain
 import UEOT.V3.FiniteCTMCPathKL
+import UEOT.V3.GirsanovPathKL
 import UEOT.V3.PAlg01
 import UEOT.V3.ReflexiveStateAugmentation
 import UEOT.V3.ReflexiveStatePathLaw
